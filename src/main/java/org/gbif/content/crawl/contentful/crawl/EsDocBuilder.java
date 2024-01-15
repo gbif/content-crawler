2e7db5cc325831b8fe27a56573b1a81ff5a1cc7e
fmendezh <federicomh@gmail.com>
2024-01-15 09:52:48 +0100

    https://github.com/gbif/content-crawler/issues/57 preserving the order of blocks

--- src/main/java/org/gbif/content/crawl/contentful/crawl/EsDocBuilder.java
+++ src/main/java/org/gbif/content/crawl/contentful/crawl/EsDocBuilder.java
@@ -104,21 +104,16 @@ public class EsDocBuilder {
     return entries;
   }
 
-  private Optional<Map<String,Object>> getBlocks(CDAEntry cdaEntry) {
+  private Optional<List<Map<String,Object>>> getBlocks(CDAEntry cdaEntry) {
     if (cdaEntry.getField("blocks") != null) {
-      Map<String,Object> blockFields = new LinkedHashMap<>();
+      List<Map<String,Object>> blockField = new LinkedList<>();
       List<CDAEntry> blocks = cdaEntry.getField("blocks");
       blocks.forEach(block ->  {
-        String blockName = block.contentType().id();
-        Object value = blockFields.get(blockName);
-        if (value == null) {
-          value = Lists.newArrayList(block.rawFields());
-        } else {
-          ((ArrayList<Map<String, Object>>)value).add(block.rawFields());
-        }
-        blockFields.put(blockName, value);
+        Map<String,Object> blockFields = new HashMap<>(block.rawFields());
+        blockFields.put("contentType", block.contentType().id());
+        blockField.add(blockFields);
       });
-      return Optional.of(blockFields);
+      return Optional.of(blockField);
     }
     return Optional.empty();
   }
